9a6324e8755f93ff7dd7dcb8edbb654c4e93035d
ibrahim mboula <mboulaibrahim@gmail.com>
2023-11-23 11:34:17 +0100

    v2 of tree

--- main.py
+++
@@ -1,9 +0,0 @@
-from nodes import create_and_connect_nodes
-
-def main():
-    nodes = create_and_connect_nodes()
-    for node in nodes:
-        print(f"Nœud {node.id}: voisins -> {[neighbor.id for neighbor in node.neighbors]}")
-
-if __name__ == "__main__":
-    main()
