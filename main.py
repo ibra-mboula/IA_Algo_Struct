758eca482adca732a62678ba1a60ffa8a012e304
ibrahim mboula <mboulaibrahim@gmail.com>
2023-11-30 09:54:45 +0100

    a star v2 - basic implementation

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def main():
     mp.set_obstacle(x=1, y=1, w=3, h=3)
     mp.set_obstacle(x=5, y=5, w=1, h=1)
     
-    mp.set_door_position(9, 0)
+    mp.set_door_position(9, 6)
 
     # Création et connexion des nœuds selon la carte
     all_nodes = nd.create_and_connect_nodes(mp)
     
     # Trouver le nœud de départ et le nœud d'arrivée
     start_node = next(node for node in all_nodes if node.id == (0, 0))  # Exemple de position de départ
-    goal_node = next(node for node in all_nodes if node.id == (9, 9))  # Exemple de position d'arrivée
+    goal_node = next(node for node in all_nodes if node.id == (9, 6))  # Exemple de position d'arrivée
 
     # Appel de l'algorithme A*
     path = nd.a_star(start_node, goal_node, mp)
@@ -27,7 +27,7 @@ def main():
         print(f"Nœud {node.id}: voisins -> {[neighbor.id for neighbor in node.neighbors]}")
         
     # Affichage des nœuds et de la map
-    plot_map_with_nodes(mp, all_nodes)
+    plot_map_with_nodes(mp, all_nodes,path)
 
 
 if __name__ == "__main__":
--- plot.py
+++ plot.py
@@ -1,25 +1,36 @@
-# plotting.py
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-def plot_map_with_nodes(map_instance, nodes):
+def plot_map_with_nodes(map_instance, nodes, path=None):
     fig, ax = plt.subplots()
+
     # Tracer les obstacles
     for i in range(map_instance.width):
         for j in range(map_instance.height):
             if not map_instance.map[i][j]:  # Si c'est un obstacle
                 ax.plot(i, j, 's', color='red')  # Dessiner un carré rouge
 
+
+
     # Tracer les nœuds et leurs connexions
     for node in nodes:
         x, y = node.id
         ax.plot(x, y, 'o', color='blue')  # Dessiner le nœud
-        
         for neighbor in node.neighbors:
             nx, ny = neighbor.id
-            line = mlines.Line2D([x, nx], [y, ny], color='gray' ,linestyle='dashed', linewidth=0.5)
+            line = mlines.Line2D([x, nx], [y, ny], color='gray', linewidth=0.5)
             ax.add_line(line)
-    
+            
+
+
+    # Tracer le chemin si fourni
+    if path:
+        for i in range(len(path) - 1):
+            start = path[i]
+            end = path[i + 1]
+            line = mlines.Line2D([start[0], end[0]], [start[1], end[1]], color='green')
+            ax.add_line(line)
+            
     # Tracer la position de la porte en jaune
     door_x, door_y = map_instance.position_door
     ax.plot(door_x, door_y, 's', color='yellow')  # Dessiner un carré jaune
