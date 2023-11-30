bcf94b5b01aff52ddb911c068ed13109efc1db90
ibrahim mboula <mboulaibrahim@gmail.com>
2023-11-30 11:11:14 +0100

    a star v4.1

--- plot.py
+++ plot.py
@@ -18,10 +18,10 @@ def plot_map_with_nodes(map_instance, nodes, paths):
     # Dessiner les nœuds et leurs connexions
     for node in nodes:
         x, y = node.id
-        ax.plot(y + 0.5, x + 0.5, marker='o', color='red', markersize=5)
+        ax.plot(y + 0.5, x + 0.5, marker='o', color='blue', markersize=5)
         for neighbor in node.neighbors:
             nx, ny = neighbor.id
-            ax.plot([y + 0.5, ny + 0.5], [x + 0.5, nx + 0.5], color='gray', linestyle='--')
+            ax.plot([y + 0.5, ny + 0.5], [x + 0.5, nx + 0.5], color='gray', linestyle='--', linewidth=0.5)
 
     # Dessiner les chemins
     for path in paths:
@@ -29,7 +29,7 @@ def plot_map_with_nodes(map_instance, nodes, paths):
             for i in range(len(path) - 1):
                 start = path[i]
                 end = path[i + 1]
-                ax.plot([start[1] + 0.5, end[1] + 0.5], [start[0] + 0.5, end[0] + 0.5], color='blue', linewidth=2)
+                ax.plot([start[1] + 0.5, end[1] + 0.5], [start[0] + 0.5, end[0] + 0.5], color='red', linewidth=2)
 
     # Configurer les axes
     ax.set_xlim(0, map_instance.width)
